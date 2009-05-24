b85a6ec75262620efee30a777e21eb578ab7dc21
Rafael Martins <rafael.mws@gmail.com>
2009-05-24 13:01:49 -0300

    Code Style

--- pool_thread.rb
+++ pool_thread.rb
@@ -2,42 +2,39 @@ require 'java'
 
 class PoolThread
 
-    def initialize (commands, simultaneously=1)
-        @pool = []
-        @waiting = commands
-        (1..simultaneously).each { new_thread }
-    end
-    
-    def new_thread
-        @pool << Thread.new do |t|
-            Thread.stop
-            system Thread.current[:call].to_s
-        end
-    end
-    
-    def run_pool
-    
-        @pool.each do |thread| 
-            waiting = @waiting.length > 0
+	def initialize (commands, simultaneously=1)
 
-            if (thread.status == "sleep" and waiting)
-                obj = @waiting[-1]
-                pos = @waiting.length - 1
-                @waiting.delete_at pos
-                thread[:call] = obj
-                thread.run
-                
-            elsif ((thread.status == nil or thread.status == false) and waiting)
-                thread.kill
-                @pool.delete(thread)
-                new_thread
-            end
-        end
+	  @pool = []
+	  @waiting = commands
+	  simultaneously.times { new_thread }
+	end
+    
+  def new_thread
 
-        
-        Thread.new { sleep 0.1; run_pool }.join if (@waiting.length > 0)
-        
+    @pool << Thread.new do |t|
+      Thread.stop
+      system Thread.current[:call].to_s
+    end
+  end
+    
+  def run_pool
+  
+    @pool.each do |thread| 
+      waiting = @waiting.length > 0
+      
+      if (thread.status == "sleep" and waiting)
+        thread[:call] = @waiting[-1]
+        @waiting.delete_at(@waiting.length - 1)
+        thread.run
+                  
+      elsif ((thread.status == nil or thread.status == false) and waiting)
+        thread.kill
+        @pool.delete(thread)
+        new_thread
+      end
     end
     
+    Thread.new { sleep 0.1; run_pool }.join if (@waiting.length > 0)      
+  end
 
 end
